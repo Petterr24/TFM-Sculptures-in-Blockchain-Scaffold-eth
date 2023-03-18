937feb52cb832e8f050d98c3b9a2d12c49dce1ea
Petterr24 <pedroo.rodri7@gmail.com>
2023-03-18 18:37:05 +0100

    Adding info when removing an user

--- packages/hardhat/contracts/UserAuthorization.sol
+++ packages/hardhat/contracts/UserAuthorization.sol
@@ -56,6 +56,7 @@ contract UserAuthorization {
     // Event for logging user deletion
     event UserRemoved(
         address indexed userAddress
+        string info
     );
 
     // Authorizes a new user
@@ -123,7 +124,7 @@ contract UserAuthorization {
         users[_userAddress].privilegeLevel = uint8(PrivilegeLevel.NONE);
 
         // Emits the event for logging the user removal
-        emit UserRemoved(_userAddress);
+        emit UserRemoved(_userAddress, "Authorized user removed!");
     }
 
     // Checks if a user has the minimum privileges to create a Record
